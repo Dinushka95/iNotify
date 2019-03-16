c65343a72051e988e46898bc08c9c9ddf3cd6797
dinushka95 <19186127+Dinushka95@users.noreply.github.com>
2019-03-16 11:03:21 +0530

    space fix

--- app/src/main/java/com/example/inotify/interfaces/RecyclerViewItemClickListener.java
+++ app/src/main/java/com/example/inotify/interfaces/RecyclerViewItemClickListener.java
@@ -3,6 +3,7 @@ package com.example.inotify.interfaces;
 import android.view.View;
 
 public interface RecyclerViewItemClickListener {
+
     void onClick(View view, int position);
 
     void onLongClick(View view, int position);
