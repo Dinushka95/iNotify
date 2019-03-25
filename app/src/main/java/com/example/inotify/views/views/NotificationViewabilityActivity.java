827d8d2860d6b352e54b5e4dcd9104b2df4646a3
dinushka95 <19186127+Dinushka95@users.noreply.github.com>
2019-03-25 06:43:13 +0530

    bug fix

--- app/src/main/java/com/example/inotify/views/views/NotificationViewabilityActivity.java
+++ app/src/main/java/com/example/inotify/views/views/NotificationViewabilityActivity.java
@@ -3,6 +3,7 @@ package com.example.inotify.views.views;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
