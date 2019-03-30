9382220a96219a2a04917f5e334d76612454c084
dinushka95 <19186127+Dinushka95@users.noreply.github.com>
2019-03-30 09:02:48 +0530

    data usage DB helper method implemented update

--- app/src/main/java/com/example/inotify/dbHelpers/DataUsageDbHelper.java
+++ app/src/main/java/com/example/inotify/dbHelpers/DataUsageDbHelper.java
@@ -3,6 +3,7 @@ package com.example.inotify.dbHelpers;
 import android.app.ActivityManager;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.TrafficStats;
 import android.util.Log;
@@ -12,6 +13,7 @@ import com.example.inotify.configs.TbNames;
 
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 import static android.content.Context.ACTIVITY_SERVICE;
 
@@ -31,5 +33,45 @@ public class DataUsageDbHelper extends MainDbHelp {
         long result = db.insert(TbNames.DATAUSAGE_TABLE, null, contentValues);
         db.close();
         return result != -1;
+    }
+
+    public String getTotalDataUsage(String date) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from " + TbNames.DATAUSAGE_TABLE + " where DATE = \"" + date + "\" ", null);
+        if (res != null) {
+            if ((res.moveToFirst())) {
+
+                db.close();
+                return res.getString(res.getColumnIndex(TbColNames.AMOUNT));
+            }
+        }
+        db.close();
+        return "";
+    }
+
+    public String getDataUsageAvg() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select SUM(" + TbColNames.AMOUNT + ") as USAGEAMOUNT from " + TbNames.DATAUSAGE_TABLE, null);
+        int total = 0;
+        int count = 0;
+        int avg;
+        if (res != null) {
+            if ((res.moveToFirst())) {
+                do {
+                    total = total + res.getInt(res.getColumnIndex("USAGEAMOUNT"));
+                    count++;
+                } while (res.moveToNext());
+            }
+        }
+        Objects.requireNonNull(res).close();
+        db.close();
+
+        try {
+            avg = total / count;
+        } catch (Exception e) {
+            return "";
+        }
+
+        return String.valueOf(avg);
     }
 }
